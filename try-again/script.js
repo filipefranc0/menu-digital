23bbcbcf6f8f539a49f5c4536b16c7850298eaee
Filipe Franco <filipefranco.dev@gmail.com>
2022-11-23 01:32:22 +0000

    first commit

---
+++ try-again/script.js
@@ -0,0 +1,11 @@
+/* var test = document.getElementById('iten')
+test.addEventListener('mouseenter', entrar)
+test.addEventListener('mouseout', sair)
+function entrar() {
+    test.innerHTML = 'oi'
+    test.style.color = 'green'
+}
+function sair (){
+
+}
+*/
